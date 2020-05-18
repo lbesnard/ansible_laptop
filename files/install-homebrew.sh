865e5b323fa218df9de0588a1dfa5f68b5955f83
lbesnard <laurent.besnard@utas.edu.au>
2020-05-18 18:13:24 +1000

    Update install-homebrew.sh
    fix install

--- files/install-homebrew.sh
+++ files/install-homebrew.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+#yes | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
