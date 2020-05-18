96c874aab2c496dd4d4707b1eb8d918f0b5e718f
lbesnard <laurent.besnard@utas.edu.au>
2020-05-18 22:52:27 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
-sudo apt-get install git
+sudo apt-get install -y git
+
 sudo apt-get install -y software-properties-common;
 sudo apt-add-repository -y ppa:ansible/ansible;
 sudo apt-get update;
-sudo apt-get install  -y ansible;
+sudo apt-get install -y ansible;
 
 ansible-pull -U https://github.com/lbesnard/ansible_laptop.git -K -i hosts
